b6f2993a7a57d5af72128bb83348072ce80a38a2
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2023-08-25 15:32:22 -0300

    Expressão lambda declarada

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 import model.Product;
 import util.PriceUpdate;
@@ -16,6 +17,8 @@ public class App {
         productsList.add(new Product("Hd Case", 80.90));
 
         productsList.forEach(System.out::println);
+
+        Consumer<Product> cons = p -> p.setPrice(p.getPrice() * 1.1);
 
         productsList.forEach(Product::nonStaticPriceUpdate);
 
